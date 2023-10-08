0fa5cde53750475cf1f076abc19330aebb06b216
govind <govindrajingle@gmail.com>
2023-10-08 14:03:05 +0530

    file upload operations

---
+++ storage-services/src/main/java/repository/StorageRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.ImageData;
+
+public interface StorageRepository extends JpaRepository<ImageData, Long>{
+	Optional<ImageData> findByName(String fileName);
+}
