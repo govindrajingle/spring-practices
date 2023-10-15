4b2069b208926e0d09beefbb844e97e72bb9ba41
govind <govindrajingle@gmail.com>
2023-10-15 15:49:01 +0530

    crud in Cloud Vendor

---
+++ rest-demo/src/main/java/com/thinkconstructive/restdemo/controller/CloudAPIService.java
@@ -0,0 +1,43 @@
+package com.thinkconstructive.restdemo.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkconstructive.restdemo.model.CloudVendor;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudAPIService {
+	CloudVendor cloudVendor;
+
+	@GetMapping("{vendorId}")
+	public CloudVendor getCloudVendorDetails(String vendorId) {
+//		return new CloudVendor("1", "om", "Pune", "12345" );
+		return cloudVendor;
+	}
+
+	@PostMapping
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Created Successfully";
+	}
+	
+	//update
+	@PutMapping
+	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Updated Successfully";
+	}
+	
+	@DeleteMapping
+	public String deleteCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = null;
+		return "Cloud Vendor Deleted Updated Successfully";
+	}
+	
+}
