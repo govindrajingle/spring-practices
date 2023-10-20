145d8a744275c785005af1231c1f315eb55cb810
Govindraj Ingle <76841913+govindrajingle@users.noreply.github.com>
2023-10-21 01:31:14 +0530

    Delete Spring + React + Postgres/springwithpostgres 190ct directory

--- Spring + React + Postgres/springwithpostgres 190ct/src/main/java/com/postgres/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.postgres.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.postgres.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-	void deleteById(Long id);
-}
